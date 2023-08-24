31a7ce5e772746f194910e24a0110a3e28a92540
Marcin Limanski <marcinlimanski@outlook.com>
2023-08-24 17:48:26 -0400

    Revert "utils: setting url as config on getBalance api"
    This reverts commit b57f1e7d0e286d2d0f9a0adfb142d189455e20cb.

--- utils/api_simple_storage.js
+++ utils/api_simple_storage.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const config = require('../config');
 
 async function getBalance() {
-    const url = config.KALEIDO_BASE_URL;
+    const url = 'https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io/instances/0xd5f8c1cee64d44adab4c7466f848252ea74db6dd/getBalance?kld-from=0xde2b1cd9d6210605e7207154f8cbd63e0a67309b';
     const headers = {
         'accept': 'application/json',
         'Authorization': config.SIMPLE_STORAGE_AUTH_HEADER
